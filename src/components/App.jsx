6c0b22837c96d3e88ab5a2bbaf086e1cc98b51a0
Vikram S <60956383+vikramoff24@users.noreply.github.com>
2020-08-31 00:42:02 +0530

    Styling Applied

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,21 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
+      <h1 className="heading" class="heading">
+        My Contacts
+      </h1>
+      <div className="card" class="card">
+        <div className="top" class="top">
+          <h2 class="name">Beyonce</h2>
           <img
+            class="circle-img"
             src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
             alt="avatar_img"
           />
         </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
+        <div className="bottom" class="bottom">
+          <p class="info">+123 456 789</p>
+          <p class="info">b@beyonce.com</p>
         </div>
       </div>
     </div>
